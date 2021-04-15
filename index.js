29d5d14bf42f36f91bba58e9aa6be68b8bbd633e
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-15 10:16:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+// function moveDodgerLeft() {
+//   let dodger = document.getElementById("dodger");
+//   document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft" ) {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+//     left > 0;
+//     dodger.style.left = `${left - 1}px`;
+//
+//   } else if (e.key === "ArrowRight") {
+//     dodger.style.left = `${left + 1}px`;
+//   }
+// })
+// }
+function moveDodgerLeft() {
+  let dodger = document.getElementById("dodger");
+  let leftNumbers = dodger.style.left.replace("px", "");
+ let left = parseInt(leftNumbers, 10);
+
+ if (left > 0) {
+   dodger.style.left = `${left - 1}px`;
+ }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  let dodger = document.getElementById("dodger");
+  let leftNumbers = dodger.style.left.replace("px", "");
+ let left = parseInt(leftNumbers, 10);
+
+ if (left > 180 ) {
+   dodger.style.left = `${left + 1}px`;
+ }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
